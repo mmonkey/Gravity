df0af43a0b00848eb8d1f9e75e089e4e8281b96d
mmonkey <mmonkey@gmail.com>
2015-08-10 22:36:44 -0500

    Initial commit

---
+++ src/main/java/com/github/mmonkey/Gravity/Gravity.java
@@ -0,0 +1,13 @@
+package com.github.mmonkey.Gravity;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = Gravity.ID, name = Gravity.NAME, version = Gravity.VERSION)
+public class Gravity {
+
+    public static final String ID = "Gravity";
+    public static final String NAME = "Gravity";
+    public static final String VERSION = "0.0.1";
+
+
+}
